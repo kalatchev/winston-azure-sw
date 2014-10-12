747c5a4ada330ab9691176eae575185217420c77
Thomas Decaux <ebuildy@gmail.com>
2014-10-12 21:16:36 +0200

    Initial commit

---
+++ test/test.js
@@ -0,0 +1,51 @@
+/*!
+ * winston-azure
+ * Copyright(c) 2014 Thomas Decaux <ebuildy@gmail.com>
+ * Apache 2.0 Licensed
+ */
+
+var should = require('should');
+
+var fs = require('fs');
+var path = process.env.HOME || (process.env.HOMEDRIVE + process.env.HOMEPATH);
+var testCredentials = JSON.parse(fs.readFileSync(path + '/.bluesky/test.json','ascii'));
+var table = require('bluesky').storage({account: testCredentials.account, key: testCredentials.key}).table('winston');
+
+var winston = require('winston');
+var winstonAzure = require('../lib/winston-azure').Azure;
+
+winston.add(Skywriter,{
+    account: testCredentials.account,
+        key: testCredentials.key,
+      table: table.name,
+      level: 'warn',
+  partition: require('os').hostname() + ':' + process.pid
+}); 
+winston.remove(winston.transports.Console);
+
+describe('winston-skywriter:', function() {
+
+  before(function() {
+  });
+
+  describe('a logger', function() {
+
+    it('should log to Azure', function(done) {
+
+      winston.warn('Warning, you are logging to Azure', 
+        {uno: 1, dos: 'two', tres: true}, 
+        function (err, level, msg, meta) {
+          should.not.exist(err);
+          level.should.equal('warn');
+          msg.should.equal('Warning, you are logging to Azure');
+          setTimeout(function() {
+            done();
+          }, 75);
+        }
+      );
+
+    });
+
+  });
+
+});

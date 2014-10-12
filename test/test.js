f4919d377d567fe6d7324cef5f23aa2efc321727
Thomas Decaux <ebuildy@gmail.com>
2014-10-12 22:01:52 +0200

    Fix test

--- test/test.js
+++ test/test.js
@@ -11,17 +11,20 @@ var testCredentials = JSON.parse(fs.readFileSync('./test/credentials.json','utf8
 var table = require('azure-storage').createTableService(testCredentials.account, testCredentials.key);
 
 var winston = require('winston');
-var winstonAzure = require('../lib/winston-azure').WinstonAzure;
 
-winston.add(winstonAzure,{
-    account: testCredentials.account,
-        key: testCredentials.key,
-      table: testCredentials.table,
-      level: 'warn',
-  partition: require('os').hostname() + ':' + process.pid
+require('../lib/winston-azure');
+
+var logger = new (winston.Logger)({
+    transports: [
+        new (winston.transports.Azure)({
+            account: testCredentials.account,
+            key: testCredentials.key,
+            table: testCredentials.table,
+            partition: require('os').hostname() + ':' + process.pid,
+            level: 'warn'
+        })
+    ]
 });
-
-winston.remove(winston.transports.Console);
 
 describe('winston-azure:', function() {
 
@@ -32,16 +35,16 @@ describe('winston-azure:', function() {
 
     it('should log to Azure', function(done) {
 
-      winston.warn('Warning, you are logging to Azure', 
-        {uno: 1, dos: 'two', tres: true}, 
-        function (err, level, msg, meta) {
-          should.not.exist(err);
-          level.should.equal('warn');
-          msg.should.equal('Warning, you are logging to Azure');
-          setTimeout(function() {
-            done();
-          }, 75);
-        }
+        logger.warn('Warning, you are logging to Azure',
+            {uno: 1, dos: 'two', tres: true},
+            function (err, level, msg, meta) {
+              should.not.exist(err);
+              level.should.equal('warn');
+              msg.should.equal('Warning, you are logging to Azure');
+              setTimeout(function() {
+                done();
+              }, 75);
+            }
       );
 
     });
